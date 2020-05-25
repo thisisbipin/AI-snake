1cab0f59276e86a666fde65379160c1defa67666
Bipin Jadav <55138889+Aurora087@users.noreply.github.com>
2020-05-25 20:28:35 +0530

    Added Some GUI

--- Snake.cpp
+++ Snake.cpp
@@ -1,28 +1,34 @@
 #include<windows.h>
 #include<iostream> 
-#include<fstream>
 #include<conio.h> 
-#include<iomanip> 
 #include<dos.h> 
-#include<math.h> 
-#include<ctype.h> 
-#include<string.h> 
-#include<stdio.h> 
+#include<string>
 #include<process.h> 
-#include<stdlib.h> 
 #include<time.h> 
 using namespace std;
 void board();
-void gotoxy(int x, int y)
+void gotoxy(short x, short y)
 {
     COORD c = { x, y };  
     SetConsoleCursorPosition(  GetStdHandle(STD_OUTPUT_HANDLE) , c);
 }
 int main()
 {
-    
+    string name;
+    system("cls");
+    cout<<"\n\n\n\n";
+    cout<<"\t---------------------------------------------\n";
+    cout<<"\t   Hi, Welcome to the Classical Snake game\n";
+    cout<<"\n";
+    cout<<"\t---------------------------------------------\n";
+    gotoxy(15,6);
+    cout<<"\t  Enter your name:";
+    cin>>name;
+    system("cls");
+    cout<<"\n\n\n\n\t\t If you want to pause the game press any \n\t\t key other than  W,A,S,D \n\n\t\t PRESS ANY KEY TO CONTINUE";
+    getch();
     int a=2,b=2,t=120,n,x,y;
-    char jump,c='o',name[10],ch='*'; //c=4 i.e) 4th ASCII character that is a diamond
+    char jump,c='0',ch='*'; //c=4 i.e) 4th ASCII character that is a diamond
     system("cls");  board();
     int score=-1; //-1 so that initial score = 0
     gotoxy(a,b); 
@@ -36,8 +42,8 @@ int main()
             {       system("cls"); 
                 //sound(random(500)); 
                 board(); 
-                gotoxy(20,25); 
-                cout<<"SCORE : "<<score<<" If you want to exit press 0.";
+                gotoxy(0,25); 
+                cout<<name<<" your SCORE is: "<<score<<" \nIf you want to exit press 0.";
                 gotoxy(a,b); //to create movement of ch 
                 cout<<ch;
                 gotoxy(x,y);//for position of food
@@ -56,8 +62,8 @@ int main()
                 jump=getch();
         } while(jump!='0');
 
-end: gotoxy(35,12); cout<<"GAME OVER ,SCORE : " <<score;
-   
+end: gotoxy(35,12); system("cls"); cout<<"\n\n\n\t\t GAME OVER, "<<name<<" your SCORE is : " <<score;
+   getch(); system("cls");
     return 0;
 }
 
